f6c603204eade9b257aa320c390853ba8ef56760
RdzCore <daniel_rod_con@hotmail.com>
2021-04-21 15:54:44 -0500

    use react suspense for better user experience

---
+++ src/components/TrendingSearchCategories/TrendigSearchCategories.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "wouter";
+
+import getTrendingGifs from "../../services/getTrendingGifs";
+
+import "./TrendingSearchCategories.css";
+
+export default function TrendingSearchCategories() {
+  const [trendingGifs, setTrendingGifs] = useState([]);
+
+  useEffect(function () {
+    getTrendingGifs().then(setTrendingGifs);
+  }, []);
+
+  return (
+    <div className="trendig-search-categories">
+      {trendingGifs.map((gif) => {
+        return (
+          <Link key={gif} href={`/gifs/${gif}`}>
+            - Gifs of {gif}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
--- src/components/TrendingSearchCategories/index.js
+++ src/components/TrendingSearchCategories/index.js
@@ -1,38 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "wouter";
-
-import getTrendingGifs from "../../services/getTrendingGifs";
+import React, { Suspense } from "react";
 
 import useNearScreen from "../../hooks/useNearScreen";
 
-import "./TrendingSearchCategories.css";
-
-function TrendingSearchCategories() {
-  const [trendingGifs, setTrendingGifs] = useState([]);
-
-  useEffect(function () {
-    getTrendingGifs().then(setTrendingGifs);
-  }, []);
-
-  return (
-    <div className="trendig-search-categories">
-      {trendingGifs.map((gif) => {
-        return (
-          <Link key={gif} href={`/gifs/${gif}`}>
-            - Gifs of {gif}
-          </Link>
-        );
-      })}
-    </div>
-  );
-}
-
+const TrendingSearchCategories = React.lazy(() =>
+  import("./TrendigSearchCategories")
+);
 export default function LazyTrending() {
   const { isNearScreen, fromRef } = useNearScreen();
 
   return (
     <div ref={fromRef}>
-      {isNearScreen ? <TrendingSearchCategories /> : null}
+      <Suspense fallback={"...Loading"}>
+        {isNearScreen ? <TrendingSearchCategories /> : null}
+      </Suspense>
     </div>
   );
 }
