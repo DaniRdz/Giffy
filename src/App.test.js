7cd0b91b15f0fe4a5ca9c7a927db923bad3b4771
RdzCore <daniel_rod_con@hotmail.com>
2021-04-22 21:04:03 -0500

    added a basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders without errors", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Recent Search Gifs/i);
+  expect(title).toBeInTheDocument();
 });
