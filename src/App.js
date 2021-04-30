cddc19a435c096254e19a1313ec3e7a30b111d2d
RdzCore <daniel_rod_con@hotmail.com>
2021-04-30 15:52:31 -0500

    created error page an added some styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Link } from "wouter";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 import SearchResults from "./pages/SearchResults";
+import ErroPage from "./pages/ErrorPage";
 
 import { GifsContextProvider } from "./context/GifsContext";
 import "./App.css";
@@ -19,10 +20,7 @@ export default function App() {
           <Route path="/" component={Home} />
           <Route path="/gifs/:keyword" component={SearchResults} />
           <Route path="/gif/:id" component={Details} />
-          <Route
-            path="/404"
-            component={() => <h1>WOOOOOOPS... SOME ERROR OCURRED</h1>}
-          />
+          <Route path="/404" component={ErroPage} />
         </GifsContextProvider>
       </section>
     </div>
---
+++ src/pages/ErrorPage/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useLocation } from "wouter";
+
+import SearchBar from "../../components/SearchBar";
+
+import "./ErrorPage.css";
+
+const gifsErrors = [
+  "8L0Pky6C83SzkzU55a",
+  "UHAYP0FxJOmFBuOiC2",
+  "l1J9EdzfOSgfyueLm",
+  "UoeaPqYrimha6rdTFV",
+];
+
+export default function ErroPage() {
+  const [path, pushLocation] = useLocation();
+
+  const randomImage = () => {
+    return `https://media.giphy.com/media/${
+      gifsErrors[Math.floor(Math.random() * gifsErrors.length) + 1]
+    }/giphy.gif`;
+  };
+
+  return (
+    <>
+      <SearchBar />
+      <div className="error-page-wrapper">
+        <h3>WOOOOOOPS... SOME ERROR OCURRED</h3>
+        <img src={randomImage()} />
+        <button onClick={() => pushLocation("/")}>Go Home</button>
+      </div>
+    </>
+  );
+}
